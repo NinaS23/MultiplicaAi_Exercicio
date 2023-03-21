2239e3dc833ecd69311dd388cdd01e9ccaef5ba2
DUDA <mariabevenuto062@gmail.com>
2023-03-21 14:21:31 -0300

    feat: num = 23

--- src/App.java
+++ src/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static void main(String[] args)  {
-       int num = 7;
-       System.out.println("Tabela de multiplicação de" + num);
+        int num = 23;
+        System.out.println("Tabela de multiplicação de" + num);
 
-       for (int i = 1; i <= 10; i++) {
-        int result = num * i;
-        System.out.println(num + " x " + i + " = " + (result));
-    }
+        for(int i = 0; i < 11; i++){
+           int calc = num * i;
+           System.out.println(num + " * " + i + " = " + calc );
+        }
     }
 }
